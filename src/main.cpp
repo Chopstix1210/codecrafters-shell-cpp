268292f0b8bf47934a02c18ebc1c5cb660f70361
Kevin Segovia <kjsegovi@gmail.com>
2025-11-26 23:26:29 -0700

    testing user input

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  std::string command;
+
   std::cout << "$ ";
+  std::getline(std::cin, command);
+
+  std::cout << command << ": command not found\n";
+
+  return 0;
 }
