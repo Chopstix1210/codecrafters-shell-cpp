a679cb50cf301d1554bdc28fca443cb65145ba4d
Kevin Segovia <kev@customer.tmpeazx1.isp.starlink.com>
2025-12-11 23:20:31 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  while (true) {
+    cout << "$ ";
+
+    string command;
+    cin >> command;
+
+    cout << command << ": command not found" << endl;
+  }
+  return 0;
+
 }
